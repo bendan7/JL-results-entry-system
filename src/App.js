81a8ad4ece74258cb4bc72fca3f13909b2f34cbe
Ben <bendan7@gmail.com>
2019-12-03 20:50:35 +0200

    work on dashboard

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Router from './Router'
 function App() {
   return (
     <div className="App" dir="RTL">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
       <Header/>
       <Router/>
     </div>
--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './Dashboard.css';
 import {Dropdown}  from 'react-bootstrap';
 import {DropdownButton}  from 'react-bootstrap';
+import Modal from 'react-modal';
 
-const leagues_data = [{id:"789", name:"ביתר ירושלים"},{id:"235",name:"הפועל תא"},{id:"134",name:"מכבי חיפה"}]
+const leagues_data = [{id:"789", name:"נוער דרום א"},{id:"235",name:"נערים מרכז ב"},{id:"134",name:"נערים צפון ג"}]
 const games_data = [
-    {id: "1234",team1name: "מכבי חיפה", team2name: "ביתר ירושלים", round: "5" ,field_Name:"טדי" , date:"25/5/2000", time: "4:00"},
-    {id: "456", team1name: "מכבי ראשלצ", team2name: "ביתר ירוחם", round: "3" ,field_Name:"יד אליהו" , date:"24/5/2000", time: "2:00"}
+    {id: "1234",team1name: "מכבי חיפה", team2name: "ביתר ירושלים", round: "5" ,field_Name:"טדי" , date:"25/5/19", time: "8:00"},
+    {id: "456", team1name: "מכבי ראשלצ", team2name: "ביתר ירוחם", round: "3" ,field_Name:"יד אליהו" , date:"24/5/19", time: "10:00"}
     ]
 
 class Dashboard extends React.Component {
@@ -26,7 +28,7 @@ class Dashboard extends React.Component {
 
     handle_leag_select(leag_name){
         this.setState ({selected_league : leag_name})
-        this.setState ({games : this.get_leagues()})
+        this.setState ({games : this.get_games()})
     }
 
     get_leagues() {
@@ -34,28 +36,31 @@ class Dashboard extends React.Component {
     }
 
     get_games() {
-        return 
+        return games_data;
     }
-
-    
 
     render(){
         return(
             <div className= "">
-                <h2>פאנל הזנת תוצאות</h2>
-                <DropdownButton size="sm" title={this.state.selected_league}>
+                <h2 style={{marginBottom:"25px"}}>פאנל הזנת תוצאות</h2>
+                <DropdownButton size="lg" title={this.state.selected_league} style={{marginBottom:"25px"}}>
                     {this.state.leagues.map((leag)=> {return <Dropdown.Item style={{fontSize:"0.7em", textAlign:"center"}} key={leag.id} onClick={()=>{this.handle_leag_select(leag.name)}} > {leag.name} </Dropdown.Item>})}
                 </DropdownButton>
-                <br/>
+                
                 <div className="d-flex align-items-center list-group " >
                     {this.state.games.map((game)=>{return (
-                    <button 
-                        type="button"
-                        style={{width:"100%", maxWidth:"500px"}} 
-                        key={game.id} 
-                        className="list-group-item list-group-item-action">
-                        <h2>{game.id}</h2>
-                        </button>
+
+                        <GameCard 
+                        key = {game.id}
+                        gameid = {game.id}
+                        team1name = {game.team1name} 
+                        team2name = {game.team2name}
+                        round = {game.round}
+                        field_Name = {game.field_Name}
+                        date = {game.date}
+                        time = {game.time}
+                        />
+
                     )})}
                 </div>
             </div>
@@ -64,5 +69,47 @@ class Dashboard extends React.Component {
         
     }
 }
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
+  Modal.setAppElement(document.getElementById('root'))
+class GameCard extends React.Component {
+
+    render(){
+        return(
+            <button
+            id = "modelgame"
+            type="button"
+            style={{width:"100%", maxWidth:"800px"}} 
+            key={this.props.id} 
+            className="list-group-item list-group-item-action"
+            //onClick= {}
+            >
+                <div>
+                    <h5>{this.props.team1name} - {this.props.team2name}</h5>
+                    <div className="d-flex justify-content-between" style={{fontSize:"0.7em"}}>
+                        <div> מחזור: {this.props.round} </div>
+                        <div>{this.props.date} - {this.props.time}</div>
+                        <div>{this.props.field_Name}</div> 
+                    </div>
+                </div>
+            </button>
+        )
+    }
+
+
+}
+
+
+
 
 export default Dashboard;
